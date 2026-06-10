655fdc6bdd6b984a7ef37b958685966d8d099c8c
Łukasz Paczos <lpaczos@duckduckgo.com>
2026-06-10 12:45:30 +0200

    Move VerificationCheckPlayStoreInstall interface to verified-installation-api

--- verified-installation/verified-installation-api/src/main/java/com/duckduckgo/verifiedinstallation/installsource/VerificationCheckPlayStoreInstall.kt
+++ verified-installation/verified-installation-api/src/main/java/com/duckduckgo/verifiedinstallation/installsource/VerificationCheckPlayStoreInstall.kt
@@ -16,25 +16,6 @@
 
 package com.duckduckgo.verifiedinstallation.installsource
 
-import com.duckduckgo.di.scopes.AppScope
-import com.squareup.anvil.annotations.ContributesBinding
-import javax.inject.Inject
-
 interface VerificationCheckPlayStoreInstall {
     fun installedFromPlayStore(): Boolean
-}
-
-@ContributesBinding(AppScope::class)
-class VerificationCheckPlayStoreInstallImpl @Inject constructor(
-    private val installSourceExtractor: InstallSourceExtractor,
-) : VerificationCheckPlayStoreInstall {
-
-    override fun installedFromPlayStore(): Boolean {
-        val installationSource = installSourceExtractor.extract()
-        return installationSource == PLAY_STORE_PACKAGE_NAME
-    }
-
-    companion object {
-        private const val PLAY_STORE_PACKAGE_NAME = "com.android.vending"
-    }
 }
